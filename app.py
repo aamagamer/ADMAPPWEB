da45bb123f36f8a0153733e1400c29ffe73aca81
alejandroadm529-beep <alejandroadm529@gmail.com>
2025-08-20 13:27:42 -0600

    back

--- app.py
+++ app.py
@@ -2901,3 +2901,10 @@ def serve_file(filename):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
+
+@app.after_request
+def no_cache(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
